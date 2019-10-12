1082fd6187d83b9a74adb3dfddbc6194d5d57e48
@Misha Krutii <mqpower1@gmail.com>
2019-10-13 00:41:43 +0300

    Add hw3

---
+++ src/hw3/Greeter.java
@@ -0,0 +1,10 @@
+package hw3;
+
+public class Greeter {
+    public static void main(String[] args) {
+        String name = "Misha";
+        String surname = "Krutii";
+        byte age = 14;
+        System.out.println("Hello!\nI am " + name + " " + surname + " and i am " + age + " y.o");
+    }
+}
